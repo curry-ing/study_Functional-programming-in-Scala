332407325fde36c1daca78905617842d56c2e6de
Sunghoon Ma <masunghoon@gmail.com>
2017-04-24 07:20:24 +0900

    2.1 MyModule - factorial

--- src/main/scala/examples/chapter2/MyModule.scala
+++ src/main/scala/examples/chapter2/MyModule.scala
@@ -5,11 +5,22 @@ class MyModule {
     if (n < 0) -n
     else n
 
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n-1, n * acc)
+    go(n, 1)
+  }
+
   private def formatAbs(x: Int) =
     s"The absolute value of $x is ${abs(x)}"
 
+  private def formatFactorial(n: Int) =
+    s"The factorial of $n is ${factorial(n)}"
+
   def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
+    println(formatFactorial(7))
   }
 }
 
