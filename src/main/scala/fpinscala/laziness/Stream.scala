f82cfe6a8a87d324d9bbd31bf3a491bb1867c7a1
Sunghoon Ma <masunghoon@gmail.com>
2017-06-14 07:53:42 +0900

    5장 스터디 업데이트 2

--- src/main/scala/fpinscala/laziness/Stream.scala
+++ src/main/scala/fpinscala/laziness/Stream.scala
@@ -56,23 +56,66 @@ sealed trait Stream[+A] {
 
   // 5.5 foldRight를 이용하여 takeWhile을 구현
   def takeWhileViaFoldRight(p: A => Boolean): Stream[A] =
-    foldRight(empty[A])((a, b) => if (p(b)) cons(a, b) else empty)
+    foldRight(empty[A])((a, b) => if (p(a)) cons(a, b) else empty)
 
   // 5.6 foldRight를 이용하여 headOption을 구현하라
-  def headOptionViaFoldRight: Option[A] = ???
+  //  def headOptionViaFoldRight: Option[A] = ???
 
   // 5.7 foldRight를 이용하여 map, filter, append, flatMap을 구현. append 메서드는 자신의 인수에 대해 엄격하지 않아야 함
   def map[B](f: A => B): Stream[B] =
-    foldRight(Empty[B])((a, b) => cons(f(a), b))
+    foldRight(empty[B])((a, b) => cons(f(a), b))
 
   def filter(f: A => Boolean): Stream[A] =
     foldRight(empty[A])((a, b) => if (f(a)) cons(a, b) else empty)
 
-  def append[B>:A](x: Stream[B]): Stream[B] =
+  def append[B >: A](x: Stream[B]): Stream[B] =
     foldRight(x)((a, b) => cons(a, x))
 
   def flatMap[B](f: A => Stream[B]): Stream[B] =
     foldRight(empty[B])((a, b) => f(a) append b)
+
+
+  // 5.13 unfold를 이용하여 map, take, takeWhile, zipWith, zipAll을 구현하라
+  // zipAll 함수는 스트림에 요소가 더 있는 한 순회를 계속해야 한다
+  // 각 스트림이 소진되었는지는 Option을 이용해 지정한다
+
+  def map2[A, B](f: A => B): Stream[B] = unfold(this) {
+    case Cons(h, t) => Some((f(h()), t()))
+    case _ => None
+  }
+
+  def take2(n: Int): Stream[A] = unfold(this, n) {
+    case (Cons(h, t), n) => if (n == 1) Some(h(), (empty, 0)) else Some(h(), (t(), n - 1))
+    case _ => None
+  }
+
+  def takeWhile2(f: A => Boolean): Stream[A] = unfold(this) {
+    case Cons(h, t) if f(h) => Some(h(), t())
+    case _ => None
+  }
+
+  def zipWith[B, C](a: Stream[B])(f: (A, B) => C): Stream[C] = unfold(this, a) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some(f(h1(), h2()), (t1(), t2()))
+    case _ => None
+  }
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] = ???
+
+  // 5.14 앞에서 작성한 함수들을 이용하여 startsWith를 구현하라. 이 함수는 한 Stream이 다른 한 Stream의 선행 순차열 (prefix: 접두사)인지 점검해야 한다.
+  // 예를 들어 Stream(1, 2, 3) startsWith Stream(1, 2) 는 true가 되어야 한다
+  def startsWith[A](s: Stream[A]): Boolean = ???
+
+  // 5.15 unfold를 이용해서 tails를 구현하라. tails는 주어진 입력 Stream과 그 후행 순차열(suffix, 접미사)들로 이루어진 스트림을 돌려준다.
+  // 예를 들어 Stream(1, 2, 3)에 대해 이 함수는 원래의 Stream(Stream(1, 2, 3), Stream(2, 3), Stream(3), Stream())을 돌려주어야 한다
+  def tails: Stream[Stream[A]]  = ???
+
+  // 5.16 tails를 일반화한 scanRight 함수를 작성하라. 이 함수는 중간 결과들의 스트림을 돌려주는 foldRight와 비슷하다
+  // scala> Stream(1, 2, 3).scanRight(0)(_ + _).toList
+  // res0: List[Int] = List(6, 5, 3, 0)
+  // 이 예는 표현식 List(1+2+3+0, 2+3+0, 3+0, 0)과 동등해야 한다. 독자의 구현은 중간 결과들을 재사용해야 한다.
+  // 즉, 요소가 n개인 Stream을 훑는 데 걸리는 시간이 항상 n에 선형 비례해야 한다. 이 함수를 unfold를 이용해서 구현할 수 있을까?
+  // 있다면 어떻게? 없다면 왜 그럴까? 이 함수를 앞에서 작성한 어떤 함수로 구현할 수는 없을까?
+  def scanRight = ???
 }
 
 case object Empty extends Stream[Nothing]
@@ -91,4 +134,11 @@ object Stream {
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
+
+  // 5.11 좀 더 일반화된 스트림 구축 함수 unfold를 작성하라
+  // 이 함수는 초기 상태 하나와 다음 상태 및 (생성된 스트림 안의)다음 값을 산출하는 함수 하나를 받아야 한다
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case Some((h, s)) => Stream.cons(h, unfold(s)(f))
+    case None => Stream.empty
+  }
 }
--- src/main/scala/fpinscala/laziness/example.sc
+++ src/main/scala/fpinscala/laziness/example.sc
@@ -1,27 +1,87 @@
 import fpinscala.laziness._
+import fpinscala.laziness.Stream._
 
 def if2[A](cond: Boolean, onTrue: () => A, onFalse: () => A): A =
-  if(cond) onTrue() else onFalse()
+  if (cond) onTrue() else onFalse()
 
 val a = 3
 if2(a < 22, () => println("a"), () => println("b"))
 
-def if3[A](cond: Boolean, onTrue: => A, onFalse: => A):A =
-  if(cond) onTrue else onFalse
+def if3[A](cond: Boolean, onTrue: => A, onFalse: => A): A =
+  if (cond) onTrue else onFalse
 
 if3(a > 22, println("a"), println("b"))
 if3(cond = false, sys.error("fail"), 3)
 
 def maybeTwice(b: Boolean, i: => Int) = if (b) i + i else 0
 
-val x = maybeTwice(true, { println("hi"); 1 + 41})
+val x = maybeTwice(true, {
+  println("hi");
+  1 + 41
+})
 
 def maybeTwice2(b: Boolean, i: => Int) = {
   lazy val j = i
   if (b) j + j else 0
 }
 
-val y = maybeTwice2(true, { println("hi"); 1 + 41})
+val y = maybeTwice2(true, {
+  println("hi");
+  1 + 41
+})
 
 
-1 +: List(2, 3)
+//
+val ones: Stream[Int] = Stream.cons(1, ones)
+
+ones.take(5).toList
+ones.exists(_ % 2 != 0)
+ones.map(_ + 1).exists(_ % 2 == 0)
+ones.takeWhile(_ == 1)
+ones.forAll(_ != 1)
+
+
+// 5.8 ones를 조금 일반화해서 주어진 값의 무한 Stream을 돌려주는 함수 constant를 구현
+def constant[A](a: A): Stream[A] = Stream.cons(a, constant(a))
+
+constant("MA").take(5).toList
+
+
+// 5.9 n에서 시작해서 n+1, n+2 등으로 이어지는 무한 정수 스트림을 생성하는 함수를 작성하라
+def from(n: Int): Stream[Int] = Stream.cons(n, from(n + 1))
+
+from(5).take(5).toList
+
+// 5.10 무한 피보나치 수 0, 1, 1, 2, 3, 5, 8, ... 으로 이루어진 무한 스트림을 생성하는 함수 fibs를 작성하라
+val fibs = {
+  def go(a: Int, b: Int): Stream[Int] = {
+    Stream.cons(a, go(b, a + b))
+  }
+
+  go(0, 1)
+}
+
+fibs.take(10).toList
+
+
+unfold(1)((x: Int) => Some((x, x + 3))) take 5 toList
+
+//unfold((0, 1))((x: Int, y: Int) => Some((x, (y, x + y)))) take 5
+
+
+// 5.12 unfold를 이용하여 fibs. from, constant, ones를 작성
+val fibs2 = unfold((0, 1)) { case (i, b) => Some(i, (b, i + b)) }
+
+fibs2 take 10 toList
+
+def from2(n: Int) = unfold(n)(n => Some(n, n+1))
+
+from2(10) take 5 toList
+
+def constant2(n: Int) = unfold(n)(x => Some(x, x))
+
+constant2(5) take 10 toList
+
+val ones2 = unfold(1)(x => Some(x, x))
+
+ones2 take 5 toList
